c5067ed292d32a6b5092a8f5de1481552c080e23
Laila <93746327+lailacodes@users.noreply.github.com>
2025-03-14 16:54:59 -0400

    Update App.js
    Made adjustments to the sidebar menu

--- Lumi/src/App.js
+++ Lumi/src/App.js
@@ -1,52 +1,92 @@
 import React, {useState} from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import HomePage from './components/Login';
 import Journal from './components/Journal';
 import Wellness from './components/Wellness';
 import Resources from './components/Resources';
 import Dashboard from './components/Dashboard';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-function Sidebar({isOpen, toggleSidebar }) {
-  return (
-    <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
-      <button className="close-btn" onClick={toggleSidebar}>
-        &times;
-        </button>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/dashboard">Dashboard</Link></li>
-        <li><Link to="/journal">Journal</Link></li>
-        <li><Link to="/wellness">Wellness</Link></li>
-        <li><Link to="/resources">Resource Hub</Link></li>
-      </ul>
-    </div>
-  );
+function Sidebar({isOpen, toggleSidebar}) {
+    return (
+        <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
+            <button className="close-btn fa fa-chevron-left" onClick={toggleSidebar}>
+            </button>
+
+            <div id="sidebar-container">
+                <ul className="nav-link-group">
+                    {/* Header space for logo */}
+                    <li className="nav-link sidebar-heading">
+                        <img src="#" alt="lumi-logo"/>
+                    </li>
+
+                    {/* Menu options */}
+                    <a href="/" className="nav-link">
+                        <div className="home-button">
+                            <span className="fa fa-home"></span>
+                            <span>Home</span>
+                        </div>
+                    </a>
+                    <a href="/dashboard" className="nav-link">
+                        <div className="dashboard-button">
+                            <span className="fa fa-dashboard"></span>
+                            <span>Dashboard</span>
+                        </div>
+                    </a>
+                    <a href="/journal" className="nav-link">
+                        <div className="journal-button">
+                            <span className="fa fa-book"></span>
+                            <span>Journal</span>
+                        </div>
+                    </a>
+                    <a href="/wellness" className="nav-link">
+                        <div className="wellness-button">
+                            <span className="fa fa-smile"></span>
+                            <span>Wellness</span>
+                        </div>
+                    </a>
+                    <a href="/resources" className="nav-link">
+                        <div className="resource-button">
+                            <span className="fa fa-info-circle"></span>
+                            <span>Resources</span>
+                        </div>
+                    </a>
+                    <a href="/login" className="nav-link">
+                        <div className="logout-button">
+                            <span className="fa fa-arrow-right-from-bracket"></span>
+                            <span>Logout</span>
+                        </div>
+                    </a>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 function App() {
-  const [isSidebarOpen, setSidebarOpen] = useState(true);
+    const [isSidebarOpen, setSidebarOpen] = useState(true);
 
-  const toggleSidebar = () =>
-    setSidebarOpen(!isSidebarOpen);
+    const toggleSidebar = () => setSidebarOpen(!isSidebarOpen);
 
-  return (
-  <BrowserRouter>
-  <div className="app-container">
-  <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar}/>
-  <div className="content">
-  <Routes>
-  <Route path="/" element={<HomePage />} />
-  <Route path="/dashboard" element={<Dashboard />} />
-  <Route path="/journal" element={<Journal />} />
-  <Route path="/wellness" element={<Wellness />} />
-  <Route path="/resources" element={<Resources />} />
-  </Routes>
-  </div>
-  </div>
-  </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="app-container">
+                <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar}/>
+                <div className={`content ${isSidebarOpen ? '' : 'collapsed'}`}>
+                    <Routes>
+                        {/*TODO: Either make dashboard the default homepage or create new home page--login can't be the home*/}
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/dashboard" element={<Dashboard/>}/>
+                        <Route path="/journal" element={<Journal/>}/>
+                        <Route path="/wellness" element={<Wellness/>}/>
+                        <Route path="/resources" element={<Resources/>}/>
+                        <Route path="/login" element={<HomePage/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
-
 
 export default App;
