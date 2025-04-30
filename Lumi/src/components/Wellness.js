29da828dbfb570f176dbef883e70afbbff34c97b
Dana Brunson <166191666+danabrunson@users.noreply.github.com>
2025-04-30 08:04:18 -0400

    Added VR exercises to Wellness page

--- Lumi/src/components/Wellness.js
+++ Lumi/src/components/Wellness.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import './Wellness.css'
 import placeholderimage from'./placeholderimg.png'
+import field from './field-scene.gif';
+
 function Wellness() {
     return (
         <><div className='card-container'>
         <div className='activity-card'>
-            <img src={placeholderimage}></img>
+            <a
+            href="/breathing_exercise/index.html"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="open-exercise-button"
+            ><img src={field}></img></a>
             <br></br>
             <br></br>
             <p>Activity description goes here...</p>
         </div><div className='activity-card'>
-                <img src={placeholderimage}></img>
+            <a
+            href="/guided walking exercise/index.html"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="open-exercise-button"
+                ><img src={placeholderimage}></img></a>
                 <br></br>
                 <br></br>
                 <p>Activity description goes here...</p>
